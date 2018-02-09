e44844161546b1db67e8b977592938cd4f7b7505
burdettelamar <BurdetteLamar@Yahoo.com>
2018-02-09 10:49:01 -0600

    Do all output at the end

--- lib/markdown_helper.rb
+++ lib/markdown_helper.rb
@@ -25,36 +25,40 @@ module MarkdownHelper
   )
 
   def self.include(template_file_path, markdown_file_path, features = DEFAULT_FEATURES)
+    output_lines = []
     File.open(template_file_path, 'r') do |template_file|
-      File.open(markdown_file_path, 'w') do |md_file|
-        md_file.puts('<!--- GENERATED FILE, DO NOT EDIT --->') if features.tag_as_generated
-        template_file.each_line do |line|
-          unless line.start_with?(FILE_SOURCE_TAG)
-            md_file.puts(line)
+      if features.tag_as_generated
+        output_lines.push('<!--- GENERATED FILE, DO NOT EDIT --->')
+      end
+      template_file.each_line do |input_line|
+        unless input_line.start_with?(FILE_SOURCE_TAG)
+          output_lines.push(input_line)
+        else
+          relative_path = input_line.sub(FILE_SOURCE_TAG, '').gsub(/[()]/, '').strip
+          include_file_path = File.join(
+              File.dirname(template_file_path),
+              relative_path,
+          )
+          file_source = File.read(include_file_path)
+          extname = File.extname(include_file_path)
+          highlight_language = features.code_block[extname.to_sym]
+          unless highlight_language
+            # Pass through unadorned.
+            output_lines.push(file_source)
           else
-            relative_path = line.sub(FILE_SOURCE_TAG, '').gsub(/[()]/, '').strip
-            include_file_path = File.join(
-                File.dirname(template_file_path),
-                relative_path,
-            )
-            file_source = File.read(include_file_path)
-            extname = File.extname(include_file_path)
-            highlight_language = features.code_block[:extname.to_sym]
-            unless highlight_language
-              # Pass through unadorned.
-              md_file.puts(file_source)
-            else
-              # Treat as code block.
-              # Label the block with its file name.
-              file_name_line = format('<code>%s</code>', File.basename(include_file_path))
-              md_file.puts(file_name_line)
-              md_file.puts("```#{highligh_language}")
-              md_file.puts(file_source)
-              md_file.puts('```')
-            end
+            # Treat as code block.
+            # Label the block with its file name.
+            file_name_line = format('<code>%s</code>', File.basename(include_file_path))
+            output_lines.push(file_name_line)
+            output_lines.push("```#{highlight_language}")
+            output_lines.push(file_source)
+            output_lines.push('```')
           end
         end
       end
+    end
+    File.open(markdown_file_path, 'w') do |md_file|
+      md_file.write(output_lines.join(''))
     end
   end
 
