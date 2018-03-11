74775cb4e83d82b833d21d8d37d2c9779e1fe6b0
BurdetteLamar <BurdetteLamar@Yahoo.com>
2018-03-11 07:53:10 -0500

    More on readme

--- lib/markdown_helper.rb
+++ lib/markdown_helper.rb
@@ -115,10 +115,6 @@ class MarkdownHelper
         end
         alt_text = match_data[1]
         relative_file_path, attributes_s = match_data[2].split(/\s?\|\s?/, 2)
-        if relative_file_path.start_with?('http')
-          output_lines.push(input_line)
-          next
-        end
         attributes = attributes_s ? attributes_s.split(/\s+/) : []
         formatted_attributes = ['']
         attributes.each do |attribute|
@@ -127,10 +123,15 @@ class MarkdownHelper
         end
         formatted_attributes_s = formatted_attributes.join(' ')
         repo_user, repo_name = repo_user_and_name
-        absolute_file_path = File.join(
-            "https://raw.githubusercontent.com/#{repo_user}/#{repo_name}/master",
-            relative_file_path,
-        )
+        absolute_file_path = nil
+        if relative_file_path.start_with?('http')
+          absolute_file_path = relative_file_path
+        else
+          absolute_file_path = File.join(
+              "https://raw.githubusercontent.com/#{repo_user}/#{repo_name}/master",
+              relative_file_path,
+          )
+        end
         line = format('<img src="%s" alt="%s"%s>', absolute_file_path, alt_text, formatted_attributes_s) + "\n"
         output_lines.push(line)
       end
--- test/expected/not_relative.md
+++ test/expected/not_relative.md
@@ -1,3 +1,3 @@
 This file contains an image path that is not relative.
 
-![google](https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_120x44dp.png | width=100)
+<img src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_120x44dp.png" alt="google" width="100">
