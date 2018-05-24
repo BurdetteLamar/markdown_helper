14fb514d23cf5b4a4fc23ab936b03bf2e8a56a34
burdettelamar <burdettelamar@yahoo.com>
2018-05-24 11:56:33 -0500

    Consistency

--- markdown/use_cases/include/include_via_api/include_via_api.rb
+++ markdown/use_cases/include/include_via_api/include_via_api.rb
@@ -1,51 +1,26 @@
-require_relative '../../use_case'
+require_relative '../include_use_case'
 
-class IncludeViaApi < UseCase
+class IncludeViaApi < IncludeUseCase
 
   def self.build
 
-    use_case_dir_path = File.absolute_path(File.dirname(__FILE__))
-
-    use_case = self.new(use_case_dir_path)
-
-    includee_file_name = 'includee.md'
-    includer_file_name = 'includer.md'
-    included_file_name = 'included.md'
-    ruby_file_name = 'include.rb'
-
-    include_command = use_case.construct_include_command(includer_file_name, included_file_name, pristine = true)
-    ruby_command = 'ruby include.rb'
-    build_command = use_case.construct_include_command(TEMPLATE_FILE_NAME, USE_CASE_FILE_NAME, pristine = true)
-
-    use_case.commands_to_execute.push(
-        include_command,
-        build_command,
-    )
+    use_case_name = File.basename(__FILE__, '.rb')
+    use_case = self.new(use_case_name)
 
     use_case.files_to_write.store(
-        includee_file_name,
+        INCLUDEE_FILE_NAME,
         <<EOT
 Text in includee file.
 EOT
     )
 
     use_case.files_to_write.store(
-        includer_file_name,
+        INCLUDER_FILE_NAME,
         <<EOT
 Text in includer file.
 
-@[:markdown](#{includee_file_name})
+@[:markdown](#{INCLUDEE_FILE_NAME})
 
-EOT
-    )
-
-    use_case.files_to_write.store(
-        ruby_file_name,
-        <<EOT
-require 'markdown_helper'
-
-markdown_helper = MarkdownHelper.new(:pristine => true)
-markdown_helper.include('#{includer_file_name}', '#{included_file_name}')
 EOT
     )
 
@@ -58,11 +33,11 @@ Use Ruby code to include files via the API.
 
 #### File To Be Included
 
-@[markdown](#{includee_file_name})
+@[markdown](#{INCLUDEE_FILE_NAME})
 
 #### Includer File
 
-@[markdown](#{includer_file_name})
+@[markdown](#{INCLUDER_FILE_NAME})
 
 #### CLI
 
@@ -71,7 +46,7 @@ Use Ruby code to include files via the API.
 Here's the command to perform the inclusion:
 
 ```sh
-#{include_command}
+#{INCLUDE_COMMAND}
 ```
 
 @[:markdown](../../pristine.md)
@@ -80,19 +55,19 @@ API
 
 ##### Ruby File
 
-@[ruby](#{ruby_file_name})
+@[ruby](#{RUBY_FILE_NAME})
 
 ##### Command
 
 ```sh
-#{ruby_command}
+#{RUBY_COMMAND}
 ```
 
 @[:markdown](../../pristine.md)
 
 #### File with Inclusion
 
-@[markdown](#{included_file_name})
+@[markdown](#{INCLUDED_FILE_NAME})
 EOT
     )
 
