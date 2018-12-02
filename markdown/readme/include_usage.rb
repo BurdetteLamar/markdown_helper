cb7589753f33517e62731ad0c70861ea71fba578
BurdetteLamar <burdettelamar@yahoo.com>
2018-12-02 17:56:32 -0600

    More on page toc

--- markdown/readme/include_usage.rb
+++ markdown/readme/include_usage.rb
@@ -3,8 +3,10 @@ require 'markdown_helper'
 template_file_path = 'highlight_ruby_template.md'
 markdown_file_path = 'highlighted_ruby.md'
 markdown_helper = MarkdownHelper.new
+# Pristine.
 markdown_helper.include(template_file_path, markdown_file_path)
-markdown_helper.pristine = true # Pristine.
+markdown_helper.pristine = true
 markdown_helper.include(template_file_path, markdown_file_path)
-markdown_helper = MarkdownHelper.new(:pristine => true) # Also pristine.
+# Also pristine.
+markdown_helper = MarkdownHelper.new(:pristine => true)
 markdown_helper.include(template_file_path, markdown_file_path)
